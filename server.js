58a7b64be93a0a974ff1fad07b512117c2c4b85a
chaudhari sunny <chaudharisunny6@gmail.com>
2025-06-06 17:13:24 +0530

    update the server

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express=require('express');
 const app=express()
 require('dotenv').config()
-const port=process.env.PORT
+// const port=process.env.PORT
 const bodyPrser=require('body-parser')
 const indexRouter=require('./router/index')
 const db=require('./model/db')
 app.use(bodyPrser.urlencoded({extended:true}))
 app.use(express.json())
 
+const serverless = require('serverless-http');
 app.use('/api/v1/',indexRouter)
-app.listen(port,()=>{
-    console.log(`server connected to ${port}`);   
-})
+// app.listen(port,()=>{
+//     console.log(`server connected to ${port}`);   
+// })
+module.exports.handler = serverless(app);
