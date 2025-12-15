4d158eab6e973bc84b6e65c5e956b9c04158c32f
chaudharisunny <chaudharisunny6@gmail.com>
2025-12-15 13:15:44 +0530

    update code commit

--- server.js
+++ server.js
@@ -8,56 +8,25 @@ const db=require('./model/db')
 const cors=require('cors')
 app.use(bodyPrser.urlencoded({extended:true}))
 app.use(express.json())
-app.use(cors())
-// const serverless = require('serverless-http');
+
+const allowedOrigins = [
+  "http://localhost:5173",               // local frontend
+  "https://hospital-food-delivery-managment-sy.vercel.app/"     // deployed frontend
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true); // Postman
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 app.use('/api/v1/',indexRouter)
 app.listen(port,()=>{
     console.log(`server connected to ${port}`);   
 })
-// module.exports.handler = serverless(app); local server 3050 and render server connected to 10000
-
-
-// require('dotenv').config();
-// const express = require('express');
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
-
-// const connectDB = require('./model/db');
-// const indexRouter = require('./router/index');
-
-// const app = express();
-// const PORT = process.env.PORT || 3050;
-
-// // Middlewares
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(cors());
-
-// // Disable mongoose buffering (prevents timeout issues)
-// mongoose.set('bufferCommands', false);
-
-// // Routes
-// app.use('/api/v1', indexRouter);
-
-// // Health check
-// app.get('/health', (req, res) => {
-//   res.status(200).json({ status: 'OK' });
-// });
-
-// // Start server ONLY after DB connects
-// const startServer = async () => {
-//   try {
-//     await connectDB(); // ⬅️ DB FIRST
-
-//     app.listen(PORT, () => {
-//       console.log(`Server running on port ${PORT}`);
-//     });
-
-//   } catch (error) {
-//     console.error('Server not started due to DB error ❌');
-//     process.exit(1);
-//   }
-// };
-
-// startServer();
